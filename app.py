de93327fa4741d0a76626c3bd4e86c349be3327d
VENU KUMAR PICHIKA <147795435+Venukumar975@users.noreply.github.com>
2025-10-01 21:58:36 +0000

    Store application in database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask , render_template,jsonify
-from database import load_jobs_from_db,load_job_from_db
+from database import load_jobs_from_db,load_job_from_db , add_application_to_db
+from flask import request
 app = Flask(__name__)
   
 @app.route('/')
@@ -19,7 +20,16 @@ def show_job(id):
     return "not found",404
   return render_template("jobpage.html",job = job)
   
-
+@app.route("/job/<id>/apply",methods =["POST"])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  # store this in the DB
+  add_application_to_db(id,data)
+  # send an email
+  # display an acknowledgement
+  return render_template("application_submitted.html",application = data,job = job)
+  
 
 if __name__ == "__main__":
   app.run(host = "0.0.0.0",debug = True)
